4977eef5957764dd924bcb6afa106a73be0d03b5
tach0817 <72219022+tach0817@users.noreply.github.com>
2020-10-02 20:34:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import os
 import traceback
@@ -5,6 +6,8 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+if not discord.opus.is_loaded():
+    discord.opus.load_opus("heroku-buildpack-libopus")
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,5 +20,26 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+
+@bot.command()
+async def fire(ctx):
+    voice_client = ctx.message.guild.voice_client
+    voice_state = ctx.author.voice
+
+    channel = voice_state.channel
+
+    await channel.connect()
+
+    voice_client = ctx.message.guild.voice_client
+
+
+    await ctx.message.attachments[0].save("tmp.mp3")
+
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生しました。")
+
+    await voice_client.disconnect()
 
 bot.run(token)
