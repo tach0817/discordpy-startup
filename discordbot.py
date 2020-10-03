3ea079ba87bcf5b0649ca6626cde63e354c29a7f
tach0817 <72219022+tach0817@users.noreply.github.com>
2020-10-03 10:53:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,168 +18,119 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.command()
 async def fire(ctx):
 #    await ctx.send('pong')
-
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
-
     channel = voice_state.channel
-
     await channel.connect()
-
     voice_client = ctx.message.guild.voice_client
-
     ffmpeg_audio_source = discord.FFmpegPCMAudio("Fire.mp3")
     voice_client.play(ffmpeg_audio_source)
-
     time.sleep(2)
-
 #    await ctx.send("再生しました。")
-
     await voice_client.disconnect()
 
 
 @bot.command()
 async def yes(ctx):
 #    await ctx.send('pong')
-
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
-
     channel = voice_state.channel
-
     await channel.connect()
-
     voice_client = ctx.message.guild.voice_client
-
     ffmpeg_audio_source = discord.FFmpegPCMAudio("Yes.mp3")
     voice_client.play(ffmpeg_audio_source)
-
     time.sleep(2)
-
 #    await ctx.send("再生しました。")
-
     await voice_client.disconnect()
 
 
 @bot.command()
 async def f1(ctx):
 #    await ctx.send('pong')
-
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
-
     channel = voice_state.channel
-
     await channel.connect()
-
     voice_client = ctx.message.guild.voice_client
-
     ffmpeg_audio_source = discord.FFmpegPCMAudio("passing1.mp3")
-
     voice_client.play(ffmpeg_audio_source)
-
     time.sleep(5)
-
 #    await ctx.send("再生しました。")
-
     await voice_client.disconnect()
 
 
 @bot.command()
 async def f2(ctx):
 #    await ctx.send('pong')
-
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
-
     channel = voice_state.channel
-
     await channel.connect()
-
     voice_client = ctx.message.guild.voice_client
-
     ffmpeg_audio_source = discord.FFmpegPCMAudio("passing2.mp3")
-
     voice_client.play(ffmpeg_audio_source)
-
     time.sleep(5)
-
 #    await ctx.send("再生しました。")
-
     await voice_client.disconnect()
 
 
 @bot.command()
 async def thomas(ctx):
 #    await ctx.send('pong')
-
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
-
     channel = voice_state.channel
-
     await channel.connect()
-
     voice_client = ctx.message.guild.voice_client
-
     ffmpeg_audio_source = discord.FFmpegPCMAudio("thomas.mp3")
-
     voice_client.play(ffmpeg_audio_source)
-
     time.sleep(5)
-
 #    await ctx.send("再生しました。")
-
     await voice_client.disconnect()
 
 
 @bot.command()
 async def yobi(ctx):
 #    await ctx.send('pong')
-
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
-
     channel = voice_state.channel
-
     await channel.connect()
-
     voice_client = ctx.message.guild.voice_client
-
     ffmpeg_audio_source = discord.FFmpegPCMAudio("yobi.mp3")
-
     voice_client.play(ffmpeg_audio_source)
-
     time.sleep(8)
-
 #    await ctx.send("再生しました。")
-
     await voice_client.disconnect()
 
 
 @bot.command()
 async def yodobashi(ctx):
 #    await ctx.send('pong')
-
     voice_client = ctx.message.guild.voice_client
     voice_state = ctx.author.voice
-
     channel = voice_state.channel
-
     await channel.connect()
-
     voice_client = ctx.message.guild.voice_client
-
     ffmpeg_audio_source = discord.FFmpegPCMAudio("yodobashi.mp3")
-
     voice_client.play(ffmpeg_audio_source)
-
     time.sleep(8)
-
 #    await ctx.send("再生しました。")
-
     await voice_client.disconnect()
 
+
+@bot.command()
+async def don(ctx):
+    voice_client = ctx.message.guild.voice_client
+    voice_state = ctx.author.voice
+    channel = voice_state.channel
+    await channel.connect()
+    voice_client = ctx.message.guild.voice_client
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("donkey.wav")
+    voice_client.play(ffmpeg_audio_source)
+    time.sleep(5)
+    await voice_client.disconnect()
 
 
 bot.run(token)
